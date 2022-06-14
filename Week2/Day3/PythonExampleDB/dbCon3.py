7ecad12b832a17ae441cf2247a9fbbe6091ddaa6
WilliamTRev <92125097+WilliamTRev@users.noreply.github.com>
2022-06-14 15:33:57 -0400

    added simple DB example as OOP

--- Week2/Day3/PythonExampleDB/dbCon3.py
+++ Week2/Day3/PythonExampleDB/dbCon3.py
@@ -1,34 +1,36 @@
 import mysql.connector as sql
 
 
-def insert():
-    """inserts and then runs Select query and prints table"""
+class Connect:
+    @staticmethod
+    def insert():
+        """inserts and then runs Select query and prints table"""
 
-    try:
-        connection = sql.connect(host="localhost",
-                                 user="root",
-                                 password="p4ssword",
-                                 db="test"
-                                 )
+        try:
+            connection = sql.connect(host="localhost",
+                                     user="root",
+                                     password="p4ssword",
+                                     db="test"
+                                     )
 
-        cursor = connection.cursor()
+            cursor = connection.cursor()
 
-        cursor.execute("INSERT INTO users VALUES(11, 'start', '2001-03-09');")
-        connection.commit()
+            cursor.execute("INSERT INTO users VALUES(11, 'start', '2001-03-09');")
+            connection.commit()
 
-        select1 = "SELECT * FROM users"
-        cursor.execute(select1)
+            select1 = "SELECT * FROM users"
+            cursor.execute(select1)
 
-        # Fetch all the records and use a for loop to print them one line at a time
-        result = cursor.fetchall()
-        for i in result:
-            print(i)
+            # Fetch all the records and use a for loop to print them one line at a time
+            result = cursor.fetchall()
+            for i in result:
+                print(i)
 
-    except sql.Error as e:
-        print(e)
+        except sql.Error as e:
+            print(e)
 
-    finally:
-        connection.close()
+        finally:
+            connection.close()
 
 
-insert()
+Connect.insert()
