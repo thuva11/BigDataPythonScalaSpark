bfcfe08d595bc63c4674f1422f7c8d0995ccb5bf
WilliamTRev <92125097+WilliamTRev@users.noreply.github.com>
2022-07-11 16:45:15 -0400

    spark-submit example

--- Week5/day5/untitled/src/main/scala/test2.scala
+++ Week5/day5/untitled/src/main/scala/test2.scala
@@ -1,5 +1,5 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
   object test2 {
   def main(args: Array[String]): Unit = {
@@ -16,8 +16,8 @@ import org.apache.spark.sql.SparkSession
     //df1.createOrReplaceTempView("people")
     //spark.sql("SELECT * from people WHERE _c1=23; ").show()
 
-    /*df1.write.mode(SaveMode.Overwrite).option("path","hdfs://localhost:9000/user/will/test10").saveAsTable("PeopleTable10000")
-    spark.sql("SELECT * FROM PeopleTable10000").show()*/
+    df1.write.mode(SaveMode.Overwrite).option("path","hdfs://localhost:9000/user/will/test10").saveAsTable("PeopleTable10000")
+    spark.sql("SELECT * FROM PeopleTable10000").show()
 
     spark.read.option("path","hdfs://localhost:9000/user/will/test10").table("PeopleTable10000").show()
     spark.sql("SELECT _c0 As name, _c1 As age FROM PeopleTable10000 where _c1>25 ").show()
